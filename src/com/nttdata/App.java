30159c546d4e7897e0dc404d62133498b776f254
alejandro <aledelarosa2@gmail.com>
2022-04-07 17:33:11 +0200

    Primera versión

---
+++ src/com/nttdata/App.java
@@ -0,0 +1,18 @@
+package com.nttdata;
+/**
+ * Proyecto Git - Formacion
+ * @author Alejandro-NTT
+ *
+ */
+public class App {
+/**
+ * Metodo principal
+ * 
+ * @param args
+ */
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+
+	}
+
+}
